26529bdcde0cf802260fe308dfa6e9ac9b5af550
Hemaprasanthi Mutyala <42854519+Hemaprasanthi@users.noreply.github.com>
2019-10-29 23:50:17 -0700

    Add files via upload

---
+++ Assignment4/index.js
@@ -0,0 +1,17 @@
+const dbRef = firebase.database().ref();
+const detailsRef = dbRef.child('details');
+detailsRef.on("child_added", function(snapshot, prevChildKey) {
+  var newPost = snapshot.val();
+});
+function send(){
+  var first = document.getElementById("firstname").value;
+  var last = document.getElementById("lastname").value;
+  var studentid = document.getElementById("sid").value;
+  var asgntnumber = document.getElementById("asgnumber").value;
+  detailsRef.push().set({
+    first: first,
+    last: last,
+    studentid: studentid,
+    asgntnumber: asgntnumber
+  });
+}
